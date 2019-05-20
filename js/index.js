6b579e8b79351e6714f9d824b6e259de5b23d381
mharley12345 <49734550+mharely12345@users.noreply.github.com>
2019-05-20 14:28:47 -0700

    1st Task

--- js/index.js
+++ js/index.js
@@ -16,18 +16,18 @@ const siteContent = {
   "main-content": {      
     "features-h4":"Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
+    "about-h4":"About",  
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",        
+    "middle-img-src": "img/mid-page-accent.jpg",   
+    "services-h4":"Services",                     
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "product-h4":"Product",                                  
     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "vision-h4":"Vision",                                    
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-  },                      
-  "contact": {
-    "contact-h4" : "Contact",        
+  },                                              
+  "contact": {                                    
+    "contact-h4" : "Contact",                     
     "address" : "123 Way 456 Street Somewhere, USA",           
     "phone" : "1 (888) 888-8888",                             
     "email" : "sales@greatidea.io",                          
@@ -46,28 +46,49 @@ ctaImg.setAttribute('src',siteContent["cta"]["img-src"])
                                                                        
 let middleImg = document.getElementById("middle-img")                  
 middleImg.setAttribute('src',siteContent["main-content"]["middle-img-src"])
-                                                                       
+                                                                        
 let navBar = document.getElementsByTagName("a");                
 navBar[0].textContent="Services";                        
 navBar[1].textContent="Product" ;                       
 navBar[2].textContent="Vision";                        
 navBar[3].textContent="Features";                             
-navBar[4].textContent="About";                        
-navBar[5].textContent="Contact";                      
-                                                      
-let mainHeading = document.querySelector("h1")        
-mainHeading.textContent="Dom " + " Is" + " Great";    
-                                                     
-let button1 = document.querySelector("button")      
-button1.textContent="Get Started";                  
-button1.style.borderColor="black";                 
-                                                  
-                                                 
-                                                             
-                                                                     
-                                                                    
-                                                                   
-                                                                  
-                                                                  
-                                                                 
+navBar[4].textContent="About";                                              
+navBar[5].textContent="Contact";                                           
+                                                                          
+let mainHeading = document.querySelector("h1")                           
+mainHeading.textContent="DOM Is Awesome";                      
+                                                                       
+let button1 = document.querySelector("button")                        
+button1.textContent="Get Started";                                   
+button1.style.borderColor="black";                                                                                                                                                                                                           
+                                                                                                                                                                                                                                                         
+let topContenth4 = document.getElementsByTagName("h4")                                                                                                                                                                                                   
+topContenth4[0].textContent="Features";                                                                                                                                                                                                                  
+topContenth4[1].textContent="About";                                                                                                                                                                                                                     
+topContenth4[2].textContent="Services";                                                                         
+topContenth4[3].textContent="Products";                                                                        
+topContenth4[4].textContent="Vision";                                                                         
+                                                                                                                                   
+let topContentP = document.getElementsByTagName("p");                                                             
+topContentP[0].textContent="Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+topContentP[1].textContent="About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."              
+topContentP[2].textContent="Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."              
+topContentP[3].textContent="Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis"               
+topContentP[4].textContent="Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."                                     
+topContentP[5].textContent="123 Way 456 Street Somewhere, USA"
+topContentP[6].textContent="1 (888) 888-8888";       
+topContentP[7].textContent="sales@greatidea.io";    
+topContentP[8].textContent="Copyright Great Idea! 2018"                                                                                                               
+let contact1 = document.querySelectorAll(".contact,h4")                                                  
+contact1[6].textContent= "Contact";                                     
+                                                                                         
+                                                                                                              
+                                                                                                             
+                                                                                                            
+                                                                                                            
+                                                                                                           
+                                                                                                          
+                                                                                                      
+                                                                                                     
+                                                                                                    
                                                                 
